1be935977e26f3c7c44967fd10c8a230b14d6bff
Vicente Santos <vicentedpsantos@gmail.com>
2019-03-22 21:03:08 -0300

    Created the DocumentsComponent and added it to Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HomepageComponent } from './homepage/homepage.component'
+import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    DocumentsComponent,
     HomepageComponent,
     AppComponent
   ],
---
+++ src/app/documents/documents.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'documents',
+  templateUrl: './documents.component.html',
+  styleUrls: ['./documents.component.sass']
+})
+export class DocumentsComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
