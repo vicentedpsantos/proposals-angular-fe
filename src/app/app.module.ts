cab75981956d35072e8cf57e5e379162456e98b8
Vicente Santos <vicentedpsantos@gmail.com>
2019-03-23 18:30:18 -0300

    Added dynamic routing to show single proposal details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
-import { ProposalNewComponent } from './proposal/proposal-new.component'
+import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    ProposalShowComponent,
     DocumentsComponent,
     HomepageComponent,
     ProposalListComponent,
---
+++ src/app/proposal/proposal-show.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'proposal-show',
+  templateUrl: './proposal-show.component.html'
+})
+export class ProposalShowComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
