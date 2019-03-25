8d3c47a3c0c31eb500642528f2c288020210a528
Vicente Santos <vicentedpsantos@gmail.com>
2019-03-25 00:31:45 -0300

    Added DocumentService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service'
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
-import { ProposalShowComponent } from './proposal/proposal-show.component'
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     NgbModule
   ],
-  providers: [],
+  providers: [
+    DocumentService
+  ],
   bootstrap: [
     AppComponent
   ]
